5d928a18a26fcae030a406cd8b91d48ab8a64f66
Derkades <robinslot@hotmail.nl>
2020-03-13 23:34:39 +0100

    auto scroll to bottom, buttons not working yet

--- resources/web/script.js
+++ resources/web/script.js
@@ -13,6 +13,27 @@ $(document).ready(function() {
         $('main').css('display', 'none');
         $('#information').css('display', 'initial');
         window.selectedContainerId = undefined;
+        $('#serverTitle').text("");
+    });
+
+    $('#buttonStart').click(function() {
+        var params = {
+            id: window.selectedContainerId
+        };
+        alert('start..');
+        $.post('start_container', params, function(){
+            alert('done!');
+        });
+    });
+
+    $('#buttonKill').click(function() {
+        var params = {
+            id: window.selectedContainerId
+        };
+        alert('stop..');
+        $.post('stop_container', params, function(){
+            alert('done!');
+        });
     });
 
     loadConsoleText();
@@ -20,10 +41,16 @@ $(document).ready(function() {
 
 function loadConsoleText() {
     if (window.selectedContainerId) {
-        $('#terminal-logs').load('/api/get_container_logs?id=' + window.selectedContainerId);
+        console.log($('#terminal-logs').text());
+        var scroll = $('#terminal-logs').text() == "Loading..";
+        $('#terminal-logs').load('/api/get_container_logs?id=' + window.selectedContainerId, null, function(){
+            if (scroll) {
+                $('#terminal-logs').scrollTop($('#terminal-logs')[0].scrollHeight);
+            }
+        });
     } else {
         $('#terminal-logs').text('Loading..');
-        setTimeout(loadConsoleText, 500);
+        setTimeout(loadConsoleText, 100);
     }
 }
 
