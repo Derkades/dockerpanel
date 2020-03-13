9a685989f91986d3452c6547c34f4a8994efd094
Derkades <robinslot@hotmail.nl>
2020-03-13 21:53:20 +0100

    start working on container selection

--- resources/web/script.js
+++ resources/web/script.js
@@ -1,9 +1,11 @@
-$(document).ready(function(){
-    $.get('/api/get_available_containers', function(data){
+$(document).ready(function() {
+    $.get('/api/get_available_containers', function(data) {
         var text = "";
-        $.each(data, function(id, name){
-            text += '<button type="button" class="list-group-item list-group-item-action" id="' + id + '">' + name + '</button>\n'
-        })
+        $.each(data, function(id, name) {
+            text += '<button type="button" class="list-group-item list-group-item-action" ';
+            text += 'id="nav-container-' + id + '" ';
+            text += 'onclick="setSelectedContainer(&quot;' + id + '&quot;, &quot;' + name + '&quot;)">' + name + '</button>\n'
+        });
         $('#collapseContainerSpoiler > .list-group').html(text);
     });
 });
@@ -12,5 +14,10 @@ function loadConsoleText(id) {
     $.get('/api/get_console_log?id=' + id, function(data) {
         alert(data);
     });
-    // $('#consoleViewPort').text()
+}
+
+function setSelectedContainer(id, name) {
+    window.selectedContainerId = id;
+    $('#nav-container-' + id).addClass("active");
+    alert("selected container set to " + id + " " + name);
 }
