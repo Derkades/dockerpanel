370e67f9436837ced804e4d47916591494e1651a
Derkades <robinslot@hotmail.nl>
2020-03-14 12:59:33 +0100

    hacky send command using socat

--- src/xyz/derkades/dockerpanel/api/SendCommand.java
+++ src/xyz/derkades/dockerpanel/api/SendCommand.java
@@ -1,19 +1,11 @@
 package xyz.derkades.dockerpanel.api;
 
-import java.io.ByteArrayInputStream;
-import java.io.Closeable;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
-import com.github.dockerjava.api.async.ResultCallback;
-import com.github.dockerjava.api.model.Frame;
-
 import xyz.derkades.dockerpanel.ApiMethod;
-import xyz.derkades.dockerpanel.App;
-import xyz.derkades.dockerpanel.ThreadBlocker;
 
 public class SendCommand extends ApiMethod {
 
@@ -35,42 +27,63 @@ public class SendCommand extends ApiMethod {
 			return;
 		}
 		
-		ThreadBlocker blocker = new ThreadBlocker();
+//		ThreadBlocker blocker = new ThreadBlocker();
+//		
+//		ResultCallback<Frame> callback = new ResultCallback<Frame>() {
+//
+//			@Override
+//			public void close() throws IOException {
+//				System.out.println("CLOSE");
+//			}
+//
+//			@Override
+//			public void onStart(Closeable closeable) {
+//				System.out.println("START");
+//			}
+//
+//			@Override
+//			public void onNext(Frame object) {
+//				System.out.println(object.toString());
+//			}
+//
+//			@Override
+//			public void onError(Throwable throwable) {
+//				throwable.printStackTrace();
+//			}
+//
+//			@Override
+//			public void onComplete() {
+//				System.out.println("STOP");
+//				blocker.done();
+//			}
+//
+//		};
+//		
+//		InputStream stream = new ByteArrayInputStream(parameters.get("command").getBytes());
+//		
+//		App.docker().attachContainerCmd(parameters.get("id"))
+//		.withStdIn(stream).withStdErr(true).withStdOut(true).withFollowStream(false)
+//		.exec(callback);
+//		
+//		blocker.block();
 		
-		ResultCallback<Frame> callback = new ResultCallback<Frame>() {
+		String id = parameters.get("id");
+		String command = parameters.get("command");
 
-			@Override
-			public void close() throws IOException {
-				System.out.println("CLOSE");
-			}
+		Process process = 
+				new ProcessBuilder()
+				.command("socat", "EXEC:\"docker attach " + id + "\",pty", "STDIN")
+				.start();
+		OutputStreamWriter writer = new OutputStreamWriter(process.getOutputStream());
+		writer.write(command + "\n");
+		writer.flush();
+		writer.close();
 
-			@Override
-			public void onStart(Closeable closeable) {
-				System.out.println("START");
-			}
-
-			@Override
-			public void onNext(Frame object) {
-				System.out.println(object.toString());
-			}
-
-			@Override
-			public void onError(Throwable throwable) {
-				throwable.printStackTrace();
-			}
-
-			@Override
-			public void onComplete() {
-				blocker.done();
-			}
-
-		};
-		
-		InputStream stream = new ByteArrayInputStream(parameters.get("command").getBytes());
-		
-		App.docker().attachContainerCmd(parameters.get("id")).withStdIn(stream).exec(callback);
-		
-		blocker.block();
+		if (process.waitFor() == 0) {
+			response.getWriter().write("ok");
+		} else {
+			response.getWriter().write("error");
+		}
 	}
 
 }
