1e28f1a30c24bf3d662280fa4034959dfd9fa056
Derkades <robinslot@hotmail.nl>
2020-03-15 16:47:47 +0100

    Fix #16

--- src/xyz/derkades/dockerpanel/App.java
+++ src/xyz/derkades/dockerpanel/App.java
@@ -16,22 +16,23 @@ import com.github.dockerjava.okhttp.OkHttpDockerCmdExecFactory;
 import com.google.gson.Gson;
 
 public class App {
-	
+
 	private static WebServer server;
 	private static DockerClient docker;
 	private static String theme;
-	
-	public static void main(String[] args) throws Exception {
-		long startTime = System.currentTimeMillis();
+
+	public static void main(final String[] args) throws Exception {
+		final long startTime = System.currentTimeMillis();
 		System.out.println("Starting.. ");
-		
+
 		System.setProperty("org.eclipse.jetty.util.log.class", "org.eclipse.jetty.util.log.StdErrLog");
 		System.setProperty("org.eclipse.jetty.LEVEL", "OFF");
 		System.setProperty("org.eclipse.jetty.util.log.announce", "false");
-		
+
 		loadTheme();
-		
+
 		Runtime.getRuntime().addShutdownHook(new Thread() {
+			@Override
 			public void run() {
 				System.out.println("Stopping server..");
 				if (server != null && !server.isStopped()) {
@@ -40,103 +41,104 @@ public class App {
 				System.out.println("bye!");
 			}
 		});
-		
-		File docksock = new File("/var/run/docker.sock");
+
+		final File docksock = new File("/var/run/docker.sock");
 		if (!docksock.exists()) {
 			System.err.println("Docker socket not found");
 			System.exit(1);
 			return;
 		}
-		
+
 		docker = DockerClientImpl.getInstance()
 			    .withDockerCmdExecFactory(new OkHttpDockerCmdExecFactory());
-		
+
 		System.out.println("Connected to docker version " + docker.versionCmd().exec().getVersion() + " on " + docker.versionCmd().exec().getOperatingSystem());
-		
+
 		server = new WebServer();
 		server.start();
-		
+
 		System.out.println("\nContainers: ");
-		for (Container container : App.getContainers()) {
+		for (final Container container : App.getContainers()) {
 			System.out.println(" - " + containerName(container));
 		}
-		
+
 		System.out.println();
-		
+
 		// Wait for webserver to start
-		
+
 		while (!server.isStarted()) {
 			try {
 				Thread.sleep(100);
-			} catch (InterruptedException e) {
+			} catch (final InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		System.out.println("Started (" + (System.currentTimeMillis() - startTime) + " ms)");
 	}
-	
+
 	public static List<Container> getContainers() {
-		String whitelist = System.getenv("CONTAINER_WHITELIST");
+		final String whitelist = System.getenv("CONTAINER_WHITELIST");
+		final List<String> allStatuses = Arrays.asList("created", "restarting", "running", "paused", "exited");
 		if (whitelist == null) {
-			return docker().listContainersCmd().exec();
+			return docker().listContainersCmd().withStatusFilter(allStatuses).exec();
 		} else {
-			return docker().listContainersCmd()
+			return docker().listContainersCmd().withStatusFilter(allStatuses)
 					.withNameFilter(Arrays.stream(whitelist.split(" ")).map((s) -> "/" + s).collect(Collectors.toList()))
 					.exec();
 		}
 	}
-	
-	public static String containerName(Container container) throws IOException {
+
+	public static String containerName(final Container container) throws IOException {
 //		return container.inspect().asJsonObject().getString("Name").substring(1);
 		return container.getNames()[0].substring(1);
 	}
-	
-	public static Container container(String id) {
-		for (Container container : getContainers()) {
+
+	public static Container container(final String id) {
+		for (final Container container : getContainers()) {
 			if (container.getId().equals(id)) {
 				return container;
 			}
 		}
 		return null;
 	}
-	
+
 	public static DockerClient docker() {
 		return docker;
 	}
-	
-	public static String toJson(Object object) {
+
+	public static String toJson(final Object object) {
 		return new Gson().toJson(object);
 	}
-	
-	public static void writeJson(HttpServletResponse response, Object object) throws IOException {
+
+	public static void writeJson(final HttpServletResponse response, final Object object) throws IOException {
 		response.getWriter().println(toJson(object));
 		response.setStatus(HttpServletResponse.SC_OK);
 		response.setContentType("text/json");
 	}
-	
+
 	private static void loadTheme() {
 		String theme = System.getenv("THEME");
 		if (theme == null) {
 			System.out.println("No theme set, using default.");
 			theme = "default";
 		}
-		
+
 		try (InputStream stream = App.class.getResourceAsStream("/themes/" + theme + ".css")){
 			if (stream == null) {
 				System.err.println("Theme not found");
 				System.exit(1);
 				return;
 			}
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		App.theme = theme;
 	}
-	
+
 	public static String getTheme() {
 		return theme;
 	}
-	
+
 }
