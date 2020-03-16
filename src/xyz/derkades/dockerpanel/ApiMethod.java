428919b89e29548cc8f84da2d51913e79f7a5517
Derkades <robinslot@hotmail.nl>
2020-03-16 23:22:54 +0100

    Add restart_container api method

--- src/xyz/derkades/dockerpanel/ApiMethod.java
+++ src/xyz/derkades/dockerpanel/ApiMethod.java
@@ -4,34 +4,36 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
-import xyz.derkades.dockerpanel.api.GetContainers;
 import xyz.derkades.dockerpanel.api.GetContainerLogs;
 import xyz.derkades.dockerpanel.api.GetContainerStatus;
+import xyz.derkades.dockerpanel.api.GetContainers;
+import xyz.derkades.dockerpanel.api.RestartContainer;
 import xyz.derkades.dockerpanel.api.SendCommand;
 import xyz.derkades.dockerpanel.api.StartContainer;
 import xyz.derkades.dockerpanel.api.StopContainer;
 
 public abstract class ApiMethod {
-	
+
 	public static final ApiMethod[] METHODS = {
 			new GetContainers(),
 			new GetContainerLogs(),
 			new GetContainerStatus(),
+			new RestartContainer(),
 			new SendCommand(),
 			new StartContainer(),
 			new StopContainer(),
 	};
-	
-	private String name;
-	
-	public ApiMethod(String name) {
+
+	private final String name;
+
+	public ApiMethod(final String name) {
 		this.name = name;
 	}
-	
+
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public abstract void call(Map<String, String> parameters, HttpServletResponse response) throws Exception;
 
 }
---
+++ src/xyz/derkades/dockerpanel/api/RestartContainer.java
@@ -0,0 +1,43 @@
+package xyz.derkades.dockerpanel.api;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.github.dockerjava.api.model.Container;
+
+import xyz.derkades.dockerpanel.ApiMethod;
+import xyz.derkades.dockerpanel.App;
+
+public class RestartContainer extends ApiMethod {
+
+	public RestartContainer() {
+		super("restart_container");
+	}
+
+	@Override
+	public void call(final Map<String, String> parameters, final HttpServletResponse response) throws Exception {
+		response.setContentType("text/plain");
+
+		if (!parameters.containsKey("id")) {
+			response.getWriter().println("Mising parameter id");
+			return;
+		}
+
+		final String id = parameters.get("id");
+		final Container container = App.container(id);
+		if (container == null) {
+			response.getWriter().print("invalid id");
+			return;
+		}
+
+		try {
+			App.docker().restartContainerCmd(container.getId()).exec();
+			response.getWriter().print("ok");
+		} catch (final IOException e) {
+			response.getWriter().print("error");
+		}
+	}
+
+}
