71f429f363ff061d7e8aa898b8a9ffbea3bfb8dc
Derkades <robinslot@hotmail.nl>
2020-05-23 16:35:52 +0200

    Use maven container for building

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
-mvn package shade:shade
+docker run --rm -u "$UID" -v "$PWD:/data" -v "$HOME/.m2:/var/maven/.m2" -e MAVEN_CONFIG=/var/maven/.m2 -w /data maven:3-adoptopenjdk-14 mvn -Duser.home=/var/maven clean package shade:shade
 docker build -t derkades/dockerpanel .
