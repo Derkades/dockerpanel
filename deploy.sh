63be525c34d4d65453225a4c3b9f6c3571270aff
Derkades <robinslot@hotmail.nl>
2020-06-20 20:27:04 +0200

    Create builder in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -ex
-docker run --rm -u "$UID" -v "$PWD:/data" -v "$HOME/.m2:/var/maven/.m2" -e MAVEN_CONFIG=/var/maven/.m2 -w /data maven:3-adoptopenjdk-14 mvn -Duser.home=/var/maven clean package shade:shade
+docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
 export DOCKER_CLI_EXPERIMENTAL=enabled
+set +e
+docker buildx rm dockerpanel_builder
+set -e
+docker buildx create --use --name dockerpanel_builder
+docker run --rm -u "$UID" -v "$PWD:/data" -v "$HOME/.m2:/var/maven/.m2" -e MAVEN_CONFIG=/var/maven/.m2 -w /data maven:3-adoptopenjdk-14 mvn -Duser.home=/var/maven clean package shade:shade
 docker buildx build -t derkades/dockerpanel --platform=linux/arm,linux/arm64,linux/amd64 . --push
+docker buildx rm dockerpanel_builder
