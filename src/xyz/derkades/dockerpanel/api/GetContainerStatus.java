e9765d64d7cc706003453cd28bd9ba4592a149b1
Derkades <robinslot@hotmail.nl>
2020-07-22 10:27:17 +0200

    API method for getting status of all containers

--- src/xyz/derkades/dockerpanel/api/GetContainerStatus.java
+++ src/xyz/derkades/dockerpanel/api/GetContainerStatus.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 
 import com.github.dockerjava.api.model.Container;
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonWriter;
 
 import xyz.derkades.dockerpanel.ApiMethod;
 import xyz.derkades.dockerpanel.App;
@@ -17,21 +19,30 @@ public class GetContainerStatus extends ApiMethod {
 
 	@Override
 	public void call(final Map<String, String> parameters, final HttpServletResponse response) throws Exception {
-		response.setContentType("text/plain");
+		if (parameters.containsKey("id")) {
+			// Show status of one container in plaintext
+			response.setContentType("text/plain");
 
-		if (!parameters.containsKey("id")) {
-			response.getWriter().println("Mising parameter id");
-			return;
+			final String id = parameters.get("id");
+			final Container container = App.container(id);
+			if (container == null) {
+				response.getWriter().print("invalid id");
+				return;
+			}
+
+			response.getWriter().print(container.getState());
+		} else {
+			// Show status of all contains in json
+			response.setContentType("text/json");
+
+			final JsonWriter writer = new Gson().newJsonWriter(response.getWriter());
+			writer.beginObject();
+			for (final Container container : App.getContainers()) {
+				writer.name(container.getId());
+				writer.value(container.getState());
+			}
+			writer.endObject();
 		}
-
-		final String id = parameters.get("id");
-		final Container container = App.container(id);
-		if (container == null) {
-			response.getWriter().print("invalid id");
-			return;
-		}
-
-		response.getWriter().print(container.getState());
 	}
 
 }
