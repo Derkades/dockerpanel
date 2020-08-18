9b0debf2747ad8e7c47d4b1224cf216c54c5d134
Derkades <robinslot@hotmail.nl>
2020-08-18 11:55:13 +0200

    Don't disable input for testing

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@
 set -ex
 docker run --rm -u "$UID" -v "$PWD:/data" -v "$HOME/.m2:/var/maven/.m2" -e MAVEN_CONFIG=/var/maven/.m2 -w /data maven:3-adoptopenjdk-14 mvn -Duser.home=/var/maven clean package shade:shade
 docker build -t derkades/dockerpanel .
-docker run --rm -v '/var/run/docker.sock:/var/run/docker.sock' -p '8080:8080' -e PORT=8080 derkades/dockerpanel
+docker run --rm -v '/var/run/docker.sock:/var/run/docker.sock' -p '8080:8080' -e PORT=8080 -e DISABLE_INPUT=false derkades/dockerpanel
