5284db01ca1ee7151f464dbe5d00b40218b21c3c
Derkades <robin+github@rkslot.nl>
2024-01-07 12:14:49 +0100

    Remove 32-bit arm build

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export DOCKER_CLI_EXPERIMENTAL=enabled
-docker buildx build -t derkades/dockerpanel --platform=linux/arm,linux/arm64,linux/amd64 . --push
+docker buildx build -t derkades/dockerpanel --platform=linux/arm64,linux/amd64 . --push
